17b9063d82491e6b992f2d79da171c4b4798678f
Steven Lawson <unluckygamblermusic@gmail.com>
2016-07-12 18:28:19 -0600

    fixing landing and book pages

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ var knex = require('../db/knex');
 var db = require('../db/api');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  knex('book').select().then(function(data){
-
-  res.render('index', { title: 'Readz', list: data });
+  res.render('index');
 
   });
-});
 
+router.get('/book', function(req, res, next){
+  knex('book').select().then(function(data){
+    res.render('book', { title: 'Readz', list: data });
+  });
+});
 module.exports = router;
